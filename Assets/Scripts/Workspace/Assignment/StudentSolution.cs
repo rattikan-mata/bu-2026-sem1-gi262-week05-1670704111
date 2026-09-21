4a97da51813b7b9762fbcfb9a1855a82a50485aa
rattikan-mata <rattikan.mata@bumail.net>
2026-09-21 16:10:33 +0700

    assignment

--- Assets/Scripts/Workspace/Assignment/StudentSolution.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution.cs
@@ -68,22 +68,73 @@ namespace Assignment
 
         public int[] AS01_SelectionSortDescending(int[] numbers)
         {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int maxIndex = i;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] > numbers[maxIndex])
+                    {
+                        maxIndex = j;
+                    }
+                }
+
+                (numbers[i], numbers[maxIndex]) = (numbers[maxIndex], numbers[i]);
+            }
             return numbers;
         }
 
         public int[] AS02_BubbleSortDescending(int[] numbers)
         {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] < numbers[j + 1])
+                    {
+                        (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
+                    }
+                }
+            }
             return numbers;
         }
 
         public int[] AS03_InsertionSortDescending(int[] numbers)
         {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int key = numbers[i];
+                int j = i - 1;
+                while (j >= 0 && numbers[j] < key)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+                numbers[j + 1] = key;
+            }
             return numbers;
         }
 
         public int AS04_FindTheSecondLargestNumber(int[] numbers)
         {
-            return 0;
+            int first = int.MinValue;
+            int second = int.MinValue;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int num = numbers[i];
+                if (num > first)
+                {
+                    second = first;
+                    first = num;
+                }
+                else if (num > second && num < first)
+                {
+                    second = num;
+                }
+            }
+
+            return second;
         }
 
         #endregion
@@ -92,7 +143,32 @@ namespace Assignment
 
         public int EX01_FindLongestConsecutiveSequence(int[] numbers)
         {
-            return 0;
+            if (numbers == null || numbers.Length == 0) return 0;
+
+            HashSet<int> numSet = new HashSet<int>(numbers);
+            int longestStreak = 0;
+
+            foreach (int num in numSet)
+            {
+                if (!numSet.Contains(num - 1))
+                {
+                    int currentNum = num;
+                    int currentStreak = 1;
+
+                    while (numSet.Contains(currentNum + 1))
+                    {
+                        currentNum++;
+                        currentStreak++;
+                    }
+
+                    if (currentStreak > longestStreak)
+                    {
+                        longestStreak = currentStreak;
+                    }
+                }
+            }
+
+            return longestStreak;
         }
 
         #endregion
